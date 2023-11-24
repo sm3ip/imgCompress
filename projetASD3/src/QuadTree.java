2a7e0b1755d9030df9a22ffd7d8bce6d87c99a47
Lucas PEREIRA <E218341C@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-11-24 10:34:04 +0100

    init done

---
+++ projetASD3/src/QuadTree.java
@@ -0,0 +1,13 @@
+public class QuadTree {
+    private int maxLum;
+    private int size;
+    private int[][] tab;
+
+    // herits from QT as the og knot
+    //TODO: constructor, getters, setters, toString,
+    //TODO: PgmToQT(read file and store to 2d array, read the array to build a QT), QTToPgm(route returns tab of data,
+    //TODO: read this array of tuples[coords pt, lum, height] and write it into the 2d array, w/ the 2d array and previous
+    //TODO: data write the PGM file)
+    //TODO: compress methods
+    //TODO: check if really a QT (simplify if needed)
+}
