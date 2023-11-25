af3bce71c1dad8e6a52eeb10ef48891e13134ab5
sm3ip <lucasomenpc@gmail.com>
2023-11-25 14:05:54 +0100

    setters and getters implemented

--- projetASD3/src/QuadTree.java
+++ projetASD3/src/QuadTree.java
@@ -9,10 +9,45 @@ public class QuadTree extends QT {
     //TODO: constructor, getters, setters, toString,
     public QuadTree(String file){
         super();
-        this.fileRoute = file;
+        setFileRoute(file);
         //TODO: do the bullshit to retrieve the other data from the file ( might need to call the function pgmToQT)
     }
-    
+
+    // getters
+
+    public int getMaxLum() {
+        return maxLum;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[][] getTab() {
+        return tab;
+    }
+
+    public String getFileRoute() {
+        return fileRoute;
+    }
+
+    // setters
+
+    private void setMaxLum(int maxLum) {
+        this.maxLum = maxLum;
+    }
+
+    private void setSize(int size) {
+        this.size = size;
+    }
+
+    private void setTab(int[][] tab) {
+        this.tab = tab;
+    }
+
+    private void setFileRoute(String fileRoute) {
+        this.fileRoute = fileRoute;
+    }
 
     //TODO: PgmToQT(read file and store to 2d array, read the array to build a QT), QTToPgm(route returns tab of data,
     //TODO: read this array of tuples[coords pt, lum, height] and write it into the 2d array, w/ the 2d array and previous
