655888210fc383dd7f2dbdffc92d382447495a77
sm3ip <lucasomenpc@gmail.com>
2023-12-03 13:47:13 +0100

    QuadTree class's comments done

--- projetASD3/src/QuadTree.java
+++ projetASD3/src/QuadTree.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileWriter;
 
+/** Represents the root of a guadtree it extends qt
+ *
+ * @see QT
+ */
 public class QuadTree extends QT {
     private int maxLum;
     private int size;
@@ -11,6 +15,10 @@ public class QuadTree extends QT {
 
     private String fileRoute; // the pgm file's location
 
+    /** Creates the QuadTree structure
+     *
+     * @param file the pgm file location
+     */
     public QuadTree(String file){
         super();
         setFileRoute(file);
@@ -22,43 +30,77 @@ public class QuadTree extends QT {
 
     // getters
 
+    /** Gets the maximum luminosity of the picture
+     *
+     * @return the max luminosity
+     */
     public int getMaxLum() {
         return maxLum;
     }
 
+    /** gets the pgm's size
+     *
+     * @return the size
+     */
     public int getSize() {
         return size;
     }
 
+    /** Gets the 2D-array containing the pgm data
+     *
+     * @return the pgm's 2D-array
+     */
     public int[][] getTab() {
         return tab;
     }
 
+    /** Gets the pgm file location
+     *
+     * @return the string wich contains the pgm location on the disk
+     */
     public String getFileRoute() {
         return fileRoute;
     }
 
     // setters
 
+    /** Sets the maximum luminosity
+     *
+     * @param maxLum an int representing a luminosity
+     */
     private void setMaxLum(int maxLum) {
         this.maxLum = maxLum;
     }
 
+    /** Sets the pgm's size
+     *
+     * @param size a positive int
+     */
     private void setSize(int size) {
         this.size = size;
     }
 
+    /** Sets the 2D-array representing the pgm's data
+     *
+     * @param tab a 2D-array composed of int
+     */
     private void setTab(int[][] tab) {
         this.tab = tab;
     }
 
+    /** Sets the pgm file's location
+     *
+     * @param fileRoute string containing a pgm file's location
+     */
     private void setFileRoute(String fileRoute) {
         this.fileRoute = fileRoute;
     }
 
-
+    /** Reads a pgm file and stores it as a int 2D-array
+     *
+     */
     private void pgmToArr(){
-        //TODO: read file and store to temporary 2d array, read the array to build a QT
+        //reading the file
         try{
             int count =0;
             int i =0;
@@ -68,24 +110,23 @@ public class QuadTree extends QT {
             while (theReader.hasNextLine()){
                 String currLine = theReader.nextLine();
                 if (count<2){
-                    //System.out.println(currLine);
+                    //Nothing matters to us before line 2
                     count+=1;
                 } else {
+                    // we split the line at each space to get nicely formatted data, we use " +" because there can be any amount
+                    // of space in between the numbers (and at least 1)
                     String[] tokens = currLine.split(" +");
                     switch (count) {
-                        case 2 -> {
-                            //System.out.println(tokens[0]);
+                        case 2 -> { // in the case where the retrieved data is the picture's size
                             this.setSize(Integer.parseInt(tokens[0]));
                             this.setTab(new int[this.getSize()][this.getSize()]);
                             count+=1;
                         }
-
-                        //TODO: create 2D array
-                        case 3 -> {
+                        case 3 -> { // in the case where the retrieved data is the picture's max luminosity
                             this.setMaxLum(Integer.parseInt(tokens[0]));
                             count+=1;
                         }
-                        default -> {
+                        default -> { // in all the other cases the line contains the pgm's pixels
                             for (String token: tokens) {
                                     int temp = Integer.parseInt(token);
                                     this.tab[i][j] = temp;
@@ -101,16 +142,27 @@ public class QuadTree extends QT {
             }
             theReader.close();
         } catch (FileNotFoundException e){
+            // if the file doesn't exist
             System.out.println(e);
         }
     }
 
+    /** overriding QT's to string function to add the picture's size and max luminosity
+     *
+     * @return a readable String containing all information needed to rebuild the quadTree
+     */
     @Override
     public String toString(){
         return this.size+":"+this.maxLum+":"+ super.toString();
     }
 
+    /** Saves the current QuadTree structure as a readable and correctly formatted file
+     *
+     * @param location the location in which the file will be saved
+     * @return true if it happened with no issue, false otherwise
+     */
     public boolean saveCurrentQT(String location){
+        // File creation
         try{
             File daFile = new File(location);
             if (daFile.createNewFile()){
@@ -122,6 +174,7 @@ public class QuadTree extends QT {
         } catch (IOException e){
             return false;
         }
+        // writing the data into the file
         try{
             FileWriter writy = new FileWriter(location);
             writy.write(this.toString());
@@ -134,7 +187,14 @@ public class QuadTree extends QT {
         return true;
     }
 
+    /** Reads a quadtree file and creates the corresponding pgm file
+     *
+     * @param location String containing the quadTree File's location
+     * @param producedLocation String containing the location inwhich the pgm will be stored
+     * @return true if achieved, false otherwise
+     */
     public static boolean qtFileToPgm(String location,String producedLocation){
+        // retrieves the quadTree data as a String
         String temp = "";
         try{
             File qtFile = new File(location);
@@ -147,31 +207,33 @@ public class QuadTree extends QT {
             System.out.println(e);
             return false;
         }
-        //TODO: process the data
-        //format is size:max Lum:QT
+        //processes the data
+        // format is size:maxlum:(V1;V2;V3;V4)
         String[] data = temp.split(":");
         int size = Integer.parseInt(data[0]);
         int maxLum = Integer.parseInt(data[1]);
         int[][] tempTab = new int[size][size];
         String[] tree = data[2].split(";");
         String whereWeAt= "1"; // keeps track to where we are in the tree
-
+        // goes through each token
         for (int i = 0; i < tree.length; i++) {
-            String buffer = "";
-            int rightPCount = 0;
+            String buffer = ""; // this token's luminosity
+            int rightPCount = 0; // amount of closing parentheses
+            // goes through each char of the token
             for (int j = 0; j < tree[i].length(); j++) {
                 if (tree[i].charAt(j)=='('){
+                    // opening parentheses means that we are going down one level
                     whereWeAt = whereWeAt + "1";
                 }else if (tree[i].charAt(j)==')'){
-                    //do nothing maybe count them
+                    // gotta count them because each one of them mean that we'll have to go up a level after processing
+                    // that token
                     rightPCount++;
-                    //whereWeAt = whereWeAt.substring(0,whereWeAt.length()-1);
                 }else {
+                    // storing the luminosity value decimal by decimal
                     buffer = buffer + tree[i].charAt(j);
                 }
             }
-            //System.out.println("buffer is "+ buffer); BUFFER HERE HAS GOOD VALUE
-            // do shit with that one number
+            // we fill the corresponding sub array with this token's luminosity
             int workingSize = size; // the size in which we can fill the 2d array
             int orX = 0; // the x value of the area origin point
             int orY = 0; // the y value of the area origin
@@ -179,38 +241,42 @@ public class QuadTree extends QT {
             for (int j = 1; j < whereWeAt.length(); j++) {
                 switch (whereWeAt.charAt(j)){
                     case '1':
+                        // in the case of a top-left child
                         workingSize = workingSize/2;
                         break;
                     case '2':
+                        // in the case of a top-right child
                         orY+= workingSize/2;
                         workingSize = workingSize/2;
                         break;
                     case '3':
+                        // in the case of a bottom-right child
                         orY+= workingSize/2;
                         orX+= workingSize/2;
                         workingSize = workingSize/2;
                         break;
                     case '4':
+                        // in the case of a bottom-left child
                         orX+= workingSize/2;
                         workingSize = workingSize/2;
                         break;
                 }
             }
-            //System.out.println("size is "+workingSize+" x is " + orX + " y is " + orY + "buffer is " + buffer + " where value is " + whereWeAt);
 
+            //fills the previously computed area
             for (int j = orX; j < orX+workingSize ; j++) {
                 for (int k = orY; k <orY+workingSize ; k++) {
                     tempTab[j][k] = Integer.parseInt(buffer);
                 }
             }
-            // take out if its a 4
+            // go up one level for each closing parentheses recorded
             for (int j = 0; j < rightPCount; j++) {
                 whereWeAt = whereWeAt.substring(0,whereWeAt.length()-1);
             }
 
             //updates last char when we get to the next token
             if (whereWeAt !=null && whereWeAt.length()>0){
-                char lastChar =whereWeAt.charAt(whereWeAt.length()-1);//= (char) (Character.getNumericValue(whereWeAt.charAt(whereWeAt.length()-1))+1);
+                char lastChar =whereWeAt.charAt(whereWeAt.length()-1);
                 whereWeAt = whereWeAt.substring(0,whereWeAt.length()-1) + (Character.getNumericValue(lastChar)+1);
             }
 
@@ -255,31 +321,39 @@ public class QuadTree extends QT {
         return true;
     }
 
-
+    /** Does the lambda compression and makes sure that the new quadtree is still a true quadtree
+     *
+     */
     public void lambdaCompr(){
         this.tree_compression_lambda(); // do the compression
         this.trueQT(); // gets it back as an actual quadTree
     }
 
-    // Rho compression methods
-    //TODO: evaluate epsilon gotta add the var in QT and the method in there aswell
-    //TODO: find the smallest espilon
-
+    /** Does the Rho compression from the root
+     *
+     * @param p the int parameter deciding of the compression's strength
+     */
     public void rhoCompr(int p){
+        // saves the amount of knots at the start
         int startAmountKnots = this.getKnot();
         int currAmountKnots = startAmountKnots;
-        System.out.println("p is" + p + " currKnot is" + currAmountKnots + " startKnot is " + startAmountKnots + " val to comp is "+ (currAmountKnots/startAmountKnots)*100);
+        // while the compression is still not enough do it on the smallest epsilon
         while (p<((float)currAmountKnots/(float)startAmountKnots)*100){
-
+            // find the tree's smallest epsilon
             StrFloatTuple smallyEpsi = this.smallestEpsi("");
+            // finds the corresponding knot
             QT tempCute = findRhoQT(smallyEpsi);
             tempCute.tree_compression_lambda();
             this.trueQT();
             currAmountKnots = this.getKnot();
-            System.out.println("p is" + p + " currKnot is" + currAmountKnots + " startKnot is " + startAmountKnots + " val to comp is "+ ((float)currAmountKnots/(float)startAmountKnots)*100);
         }
     }
 
+    /** Finds a quadTree root through a path
+     *
+     * @param smallyEpsi contains the pathway to the knot
+     * @return returns the searched quadTree
+     */
     private QT findRhoQT(StrFloatTuple smallyEpsi) {
         String smollerPath = smallyEpsi.getPathway();
         QT tempCute = this;
