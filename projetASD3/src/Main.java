ab25b10ae2e0c34fdb400bc23494568edd447e63
sm3ip <lucasomenpc@gmail.com>
2023-12-07 11:35:19 +0100

    main cleaned up and everything working

--- projetASD3/src/Main.java
+++ projetASD3/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.util.*;
 
 public class Main {
@@ -8,39 +9,40 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         if (args.length == 0){
             // menu part
+            String menuMessage = """
+                    What do you wanna do (press the number before the action to choose)?
+                     1- Choose a pgm to load amongst a list (you'll have to provide the absolute path to the directory containing those pgms).
+                     2- Apply a lambda compression on the currently loaded pgm and create its .qt file (you'll have to provide its name).
+                     3- Apply a rho compression on the currently loaded pgm and create its.qt file (you'll have to provide its name and the parameter p).
+                     4- Generate the pgm from the previously created .qt file (can also create a pgm from a provided and well-built .qt file).
+                     5- Show the compression statistics ( if you haven't reloaded a pgm in between multiple compressions the stats will take it into account).
+                     Any other input will result in the termination of this application.""";
             boolean wantsToGoOn = true; // continues the loop
             String currentFile = ""; // the pgm being taken care of
             String lastQtLocation = ""; // the last saved .qt
-            String comprDone =""; // all the compr done
-            Quadtree loadedQT = null; // the soft's qt
+            String comprDone =""; // all the compressions done
+            Quadtree loadedQT = null; // the software's stored qt
             int startAmountNode = 0;
             while(wantsToGoOn){
-                System.out.println("Currently loaded pgm : " + currentFile);
-                System.out.println("What do you wanna do (press the number before the action to choose)?" +
-                        "\n 1- Choose a pgm to load amongst a list (you'll have to provide the absolute path to the directory containing those pgms)." +
-                        "\n 2- Apply a lambda compression on the currently loaded pgm and create its .qt file (you'll have to provide its name)." +
-                        "\n 3- Apply a rho compression on the currently loaded pgm and create its.qt file (you'll have to provide its name and the parameter p)." +
-                        "\n 4- Generate the pgm from the previously created .qt file (can also create a pgm from a provided and well-built .qt file)." +
-                        "\n 5- Show the compression statistics ( if you haven't reloaded a pgm in between multiple compressions the stats will take it into account)." +
-                        "\n Any other input will result in the termination of this application.");
+                // show the menu
+                System.out.println("Currently loaded pgm : " + currentFile + "\n" + menuMessage);
+                // retrieve the answer
                 int answer = Integer.parseInt(reader.nextLine());
-                switch (answer){
-                    case 1:
+                switch (answer){ // cases depending on the answer
+                    case 1: // load a pgm
                         System.out.println("Please provide your pgm directory's absolute path : ");
                         File pgsDir = new File(reader.nextLine());
                         if (pgsDir.exists()){
-
                             String[] pgmsfiles = pgsDir.list();
-
                             System.out.println("Press the number appearing before the wanted file");
                             int countPgs = 0;
                             for(int i = 0; i< Objects.requireNonNull(pgmsfiles).length; i++){
-                                if (pgmsfiles[i].endsWith(".pgm")){
+                                if (pgmsfiles[i].endsWith(".pgm")){ // show all pgm files in the directory
                                     countPgs++;
                                     System.out.println(i+" : "+ pgmsfiles[i]);
                                 }
                             }
-                            if (countPgs >0){
+                            if (countPgs >0){ // makes sure that the user chooses the correct option
                                 String selectedPgm = "";
                                 while (selectedPgm.isEmpty()){
                                     int tempChoice = Integer.parseInt(reader.nextLine());
@@ -51,7 +53,7 @@ public class Main {
                                     }
                                 }
                                 // load it now
-                                currentFile = pgsDir.getAbsolutePath()+"\\"+selectedPgm;
+                                currentFile = pgsDir.getAbsolutePath()+ System.getProperty("file.separator") +selectedPgm;
                                 try{
                                     loadedQT = new Quadtree(currentFile);
                                     startAmountNode = loadedQT.getNodes();
@@ -70,7 +72,7 @@ public class Main {
                             System.out.println("The provided path doesn't seem to exist");
                         }
                         break;
-                    case 2:
+                    case 2: // if asked to do a lambda compression
                         if (!currentFile.isEmpty()) {
                             // lambda compression
                             loadedQT.compressLambda();
@@ -82,7 +84,7 @@ public class Main {
                             System.out.println("No pgm image where loaded, please do so before proceeding with a compression");
                         }
                         break;
-                    case 3:
+                    case 3: // if asked to do a rho compression
                         if (!currentFile.isEmpty()){
                             int p=-1;
                             while (p<1 || p>100){
@@ -118,8 +120,8 @@ public class Main {
                             while (filename.isEmpty()) {
                                 System.out.println("Please provide the name with which you wanna save the pgm file :");
                                 filename = reader.nextLine();
-                                if (Quadtree.qtFileToPgm(qtLoc, System.getProperty("user.dir") + "\\" + filename + ".pgm")) {
-                                    System.out.println("Your pgm has successfully been generated at : " + System.getProperty("user.dir") + "\\" + filename + ".pgm");
+                                if (Quadtree.qtFileToPgm(qtLoc, System.getProperty("user.dir") + System.getProperty("file.separator") + filename + ".pgm")) {
+                                    System.out.println("Your pgm has successfully been generated at : " + System.getProperty("user.dir") + System.getProperty("file.separator") + filename + ".pgm");
                                 } else {
                                     filename = "";
                                     System.out.println("We couldn't save with this filename, please try again.");
@@ -145,11 +147,7 @@ public class Main {
         }else {
             //timing
             long startTime = System.currentTimeMillis();
-
             // part where the soft works by itself || the first param is the filename, the second is p
-            // DONE: check if params are good; load pgm; do lambda;
-            //TODO:   create .qt; create .pgm; read out loud stats;
-            //TODO: reload pgm; do rho with p (read out loud progress); create .qt; create. pgm; read out loud stats;
             int p = Integer.parseInt(args[1]);
             if(p<1 || p>100){
                 System.out.println("The value of p isn't between 1 and 100; p = "+ p);
@@ -161,16 +159,16 @@ public class Main {
                     // lambda compression
                     temp.compressLambda();
                     int nbNodeAfterLamb = temp.getNodes();
-                    new File(System.getProperty("user.dir") + "\\QT").mkdirs(); //(would be /QT on linux)
-                    if (temp.saveCurrentQT(System.getProperty("user.dir") + "\\QT\\lambda.qt")) {
-                        new File(System.getProperty("user.dir") + "\\PGM").mkdirs(); //(would be /PGM on linux)
-                        Quadtree.qtFileToPgm(System.getProperty("user.dir") + "\\QT\\lambda.qt", System.getProperty("user.dir") + "\\PGM\\lambda.pgm");
+                    new File(System.getProperty("user.dir") +System.getProperty("file.separator") +"QT").mkdirs(); //(would be /QT on linux)
+                    if (temp.saveCurrentQT(System.getProperty("user.dir")+ System.getProperty("file.separator") + "QT"+System.getProperty("file.separator")+"lambda.qt")) {
+                        new File(System.getProperty("user.dir") +System.getProperty("file.separator")+ "PGM").mkdirs(); //(would be /PGM on linux)
+                        Quadtree.qtFileToPgm(System.getProperty("user.dir") + System.getProperty("file.separator")+ "QT"+System.getProperty("file.separator")+"lambda.qt", System.getProperty("user.dir") +System.getProperty("file.separator")+ "PGM"+System.getProperty("file.separator")+"lambda.pgm");
                         // rho compression
                         temp = new Quadtree(args[0]);
                         temp.compressRho(p);
                         int nbNodeAfterRho = temp.getNodes();
-                        if (temp.saveCurrentQT(System.getProperty("user.dir") + "\\QT\\rho.qt")) {
-                            Quadtree.qtFileToPgm(System.getProperty("user.dir") + "\\QT\\rho.qt", System.getProperty("user.dir") + "\\PGM\\rho.pgm");
+                        if (temp.saveCurrentQT(System.getProperty("user.dir") + System.getProperty("file.separator")+"QT"+System.getProperty("file.separator")+"rho.qt")) {
+                            Quadtree.qtFileToPgm(System.getProperty("user.dir") + System.getProperty("file.separator")+"QT"+System.getProperty("file.separator")+"rho.qt", System.getProperty("user.dir") +System.getProperty("file.separator")+ "PGM"+System.getProperty("file.separator")+"rho.pgm");
                             System.out.println("Compression stats : \n Lambda : \n - original number of nodes : " + nbNodeBefore + " \n - current number of nodes : " + nbNodeAfterLamb
                                     + "\n - Compression : " + ((float) nbNodeAfterLamb / (float) nbNodeBefore) * 100 + "% \n Rho : \n - original number of nodes : "
                                     + nbNodeBefore + "\n - current number of nodes : " + nbNodeAfterRho + "\n - compression : " + ((float) nbNodeAfterRho / (float) nbNodeBefore) * 100
@@ -190,54 +188,6 @@ public class Main {
             long endTime = System.currentTimeMillis();
             System.out.println("Time taken : "+ (endTime-startTime)+"ms");
         }
-
-
-
-        /** way to update progress on rho
-         *              System.out.print("test");
-         *             Thread.sleep(1000);
-         *             System.out.print("\b\b\b\b");
-         *             System.out.print("HEYOOO");
-         */
-        //System.out.println(System.getProperty("user.dir")); current dir
-        //System.out.println(args[0]+args[1]); arguments
-        //QuadTree temp = new QuadTree("/comptes/E218341C/Documents/l3/s1/ASD3/projet/imgCompress/projetASD3/src/boat.pgm"); LINUX version
-
-        //QuadTree temp = new QuadTree("C:\\Users\\lucas\\Documents\\CMI_etudes\\L3\\ASD3\\project\\imgCompress\\projetASD3\\src\\boat.pgm");
-        //System.out.println(temp.toString());
-        //System.out.println("Nodes before" +temp.getKnot());
-        //temp.rhoCompr(80);
-        //System.out.println("KNOT after"+temp.getKnot());
-
-
-
-        //System.out.println(temp.toString());
-        //if (temp.saveCurrentQT("C:\\Users\\lucas\\Documents\\CMI_etudes\\L3\\ASD3\\project\\imgCompress\\projetASD3\\src\\QTtest1.qt")){
-        //    QuadTree.qtFileToPgm("C:\\Users\\lucas\\Documents\\CMI_etudes\\L3\\ASD3\\project\\imgCompress\\projetASD3\\src\\QTtest1.qt",
-        //            "C:\\Users\\lucas\\Documents\\CMI_etudes\\L3\\ASD3\\project\\imgCompress\\projetASD3\\src\\producedPGMtest1.pgm");
-        //}
-          /*
-           for (int i =0; i< temp.getSize(); i++){
-            for (int j=0; j<temp.getSize(); j++){
-                System.out.print(temp.getTab()[i][j]);
-                System.out.print("  ");
-            }
-            System.out.println(" ");
-        }
-
-        int q1=3;
-        int q2=2;
-        int q3=16;
-        int q4=30;
-        float lambda = (float) Math.exp(0.25*((Math.log(0.1+q1))+(Math.log(0.1+q2))+(Math.log(0.1+q3))+(Math.log(0.1+q4))));
-        System.out.println(Math.abs(lambda-q1));
-        System.out.println(Math.abs(lambda-q2));
-        System.out.println(Math.abs(lambda-q3));
-        System.out.println(Math.abs(lambda-q4));
-        int lamb = (int) Math.round(Math.exp(0.25*((Math.log(0.1+q1))+(Math.log(0.1+q2))+(Math.log(0.1+q3))+(Math.log(0.1+q4)))));
-        System.out.println(lamb);
-        */
-
     }
 
     private static String saveQtWithFilename(Scanner reader, String lastQtLocation, Quadtree loadedQT) {
@@ -245,9 +195,9 @@ public class Main {
         while (filename.isEmpty()){
             System.out.println("Please provide the name with which you wanna save the quadtree file :");
             filename = reader.nextLine();
-            if (loadedQT.saveCurrentQT(System.getProperty("user.dir")+"\\"+filename+".qt")){
-                System.out.println("Your quadtree file has successfully been saved at : " + System.getProperty("user.dir")+"\\"+filename+".qt");
-                lastQtLocation = System.getProperty("user.dir")+"\\"+filename+".qt";
+            if (loadedQT.saveCurrentQT(System.getProperty("user.dir")+System.getProperty("file.separator")+filename+".qt")){
+                System.out.println("Your quadtree file has successfully been saved at : " + System.getProperty("user.dir")+System.getProperty("file.separator")+filename+".qt");
+                lastQtLocation = System.getProperty("user.dir")+System.getProperty("file.separator")+filename+".qt";
             } else {
                 filename = "";
                 System.out.println("We couldn't save with this filename, please try again.");
