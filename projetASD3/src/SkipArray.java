da9dc1caf140f2c643983d8c647b37d2a9ab6b4c
sm3ip <lucasomenpc@gmail.com>
2023-12-12 11:06:46 +0100

    SkipArray class cleaned up

--- projetASD3/src/SkipArray.java
+++ projetASD3/src/SkipArray.java
@@ -1,11 +1,19 @@
 public class SkipArray {
+    // an array used to store the positions at which you want to store a new element in a skip list (keeps the good order aswell)
     SkipNodeList first, last;
 
+    /** simple constructor taking no argument
+     *
+     */
     public SkipArray(){
         this.first = null;
         this.last = null;
     }
 
+    /** Adds a new node to the list at the first place
+     *
+     * @param sk a skipNode (skip-list node) to add to this array
+     */
     public void addIn(SkipNode sk){
         if (this.last ==null && this.first == null){
             this.first = new SkipNodeList(sk);
