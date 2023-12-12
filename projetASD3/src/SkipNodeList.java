6257ab04babf72ddb2ce963e1b79732494c09ada
sm3ip <lucasomenpc@gmail.com>
2023-12-12 10:57:55 +0100

    SkipNodeList class cleaned up

--- projetASD3/src/SkipNodeList.java
+++ projetASD3/src/SkipNodeList.java
@@ -1,7 +1,12 @@
 public class SkipNodeList {
+    // A node of the skip array class, it has its own skipnode value and a pointer towards the next element of the array
     SkipNode self;
     SkipNodeList next;
 
+    /** Constructor taking as a parameter the value that you want to store
+     *
+     * @param skip a Skipnode
+     */
     public SkipNodeList(SkipNode skip){
         this.self = skip;
         this.next = null;
